3f987505b737a4cf658e2d0db883b54c38883780
aazaff <aazaff@gmail.com>
2015-12-01 12:07:57 -0600

    Update cullMatrix.R

--- cullMatrix.R
+++ cullMatrix.R
@@ -57,3 +57,47 @@ diversityFlag <- function(x, minDiversity) {
  	}
  	flag
 }
+
+# Alternative to cullMatrix( ) that does not thrown an error, but returns a single NA
+softCull <- function(x, minOccurrences=2, minDiversity=2) {
+	y <- x
+	while (diversityFlag(y, minDiversity) | occurrencesFlag(y, minOccurrences)) {
+		y <- softTaxa(y, minOccurrences)
+		if (is.na(y)==TRUE) {
+			return(NA)
+			}
+		y <- softSamples(y, minDiversity)
+		if (is.na(y)==TRUE) {
+			return(NA)
+			}
+		}
+	return(y)
+	}
+	
+# Dependency of softCull()
+softTaxa <- function(x, minOccurrences) {
+	PA <- x
+ 	PA[PA>0] <- 1
+ 	occurrences <- apply(PA, MARGIN=2, FUN=sum)
+ 	aboveMinimumOccurrences <- occurrences >= minOccurrences
+ 	y <- x[ ,aboveMinimumOccurrences]
+ 	if (length(y)==0) {
+ 		y<-NA;
+ 		return(NA)
+ 		}
+ 	return(y)
+	}
+
+# Dependency of softCull()
+softSamples <- function(x, minDiversity) {
+	PA <- x
+ 	PA[PA>0] <- 1
+ 	diversity <- apply(PA, MARGIN=1, FUN=sum)
+ 	aboveMinimumDiversity <- diversity >= minDiversity
+ 	y <- x[aboveMinimumDiversity, ]
+ 	if (length(y[,1])==0) {
+ 		y<-NA;
+ 		return(NA)
+ 		}
+ 	return(y)
+	}
