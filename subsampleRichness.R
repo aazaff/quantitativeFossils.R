5bb103ea9a35d8ed6f2e90d8a68e9e671a33c7d3
aazaff <azaffos@wisc.edu>
2015-12-04 12:40:55 -0600

    Added new subsample function

--- subsampleRichness.R
+++ subsampleRichness.R
@@ -202,3 +202,23 @@ multicoreIndividuals<-function(Abundance,Quota,Trials=1000,Cores=4) {
 		}
 	return(mean(Richness))
 	}
+
+# A variant of subsamplingIndividuals() that resamples with replacement
+# for any samples that have diversities below the Quota.
+# A function for resampling by a fixed number of individuals.
+# This allows for diversity to be lower than the Quota.
+# This is a non-standard procedure that is not recommended for general use.
+resampleIndividuals<-function(Abundance,Quota,Trials=100) {
+	Richness<-vector("numeric",length=Trials)
+	Abundance<-Abundance[Abundance>0]
+	Pool<-rep(1:length(Abundance),times=Abundance)
+	if (length(unique(Pool)) < Quota) {
+		Subsample<-sample(Pool,Quota,replace=TRUE)
+		Richness[i]<-length(unique(Subsample))
+		}
+	for (i in 1:Trials) {
+		Subsample<-sample(Pool,Quota,replace=FALSE)
+		Richness[i]<-length(unique(Subsample))
+		}
+	return(mean(Richness))
+	}
