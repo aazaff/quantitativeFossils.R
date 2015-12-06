eb59473efd3207da3e4dea764b394db9b1f843c2
aazaff <azaffos@wisc.edu>
2015-12-06 16:03:15 -0600

    Basic Statistics

---
+++ basicStatistics.R
@@ -0,0 +1,9 @@
+# Least inverse squares M Estimator
+estimateMean<-function(MeansVector,ErrorsVector) {
+	SquaredErrors<-ErrorsVector^2
+	Numerator<-MeansVector/SquaredErrors
+	InverseErrors<-sum(1/SquaredErrors)
+	Mean<-sum(Numerator)/InverseErrors
+	Error<-1/sqrt(InverseErrors)
+	return(c(Mean,Error))
+	}
