6059358b41c8800577163b745d24a3604c71f8c2
aazaff <azaffos@wisc.edu>
2015-12-07 12:02:58 -0600

    Added new functions

--- basicStatistics.R
+++ basicStatistics.R
@@ -7,3 +7,17 @@ mestimateMean<-function(MeansVector,ErrorsVector) {
 	Error<-1/sqrt(InverseErrors)
 	return(c(Mean,Error))
 	}
+
+# Dependency of normalOverlap()
+findDensity<-function(x,Mean1,Mean2,StdDev1,StdDev2) {
+    Density1<-dnorm(x,mean=Mean1,sd=StdDev1)
+    Density2<-dnorm(x,mean=Mean2,sd=StdDev2)
+    pmin(Density1,Density1)
+	}
+
+# Find the overlap of the mestimated distributions
+normalOverlap<-function(MeansVector1,ErrorsVector1,MeansVector2,ErrorsVector2) {
+	FirstDist<-mestimateMean(MeansVector1,ErrorsVector1)
+	SecondDist<-mestimateMean(MeansVector2,ErrorsVector2)
+	return(integrate(findDensity,-Inf,Inf,Mean1=FirstDist[1],Mean2=SecondDist[1],StdDev1=FirstDist[2],StdDev=FirstDist[3])
+	}
